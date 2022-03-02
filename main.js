972aed3979953cf130fec1d223a33dab9d0ff2d2
parikshit_holey <parikshitholey20@gmail.com>
2022-03-02 20:01:23 +0530

    done
    done

---
+++ main.js
@@ -0,0 +1,15 @@
+const add = require("./add");
+const substract = require("./substract");
+const divide = require("./divide");
+const multiply=require("./multiply.js");
+
+
+console.log(add(10,5));
+console.log(divide(100,50));
+
+let sub = substract(70,15);
+console.log(sub);
+
+
+let multi = multiply.multiply(10,2);
+console.log(multi);
